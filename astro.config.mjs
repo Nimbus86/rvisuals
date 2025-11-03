f46d0999825915102e234b1d9f339e91b27dc3a4
Nimbus86 <dimi.ram@hotmail.com>
2025-11-03 08:52:43 +0100

    Fix asset paths, CSS import, and navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Nimbus86.github.io',
-  base: '/rvisuals',
+    site: 'https://rvisuals.netlify.app/',
+  base: '/',
 });
